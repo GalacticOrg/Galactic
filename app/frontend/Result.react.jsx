2d0b14a07361cad621e05c455c71fc7487303030
Eoin McMillan <eoinmcmillan@gmail.com>
2016-05-21 16:28:31 -0700

    safe commit pre pull

--- app/frontend/Result.react.jsx
+++ app/frontend/Result.react.jsx
@@ -13,7 +13,12 @@ class Result extends React.Component {
 
       <Grid className="resultNodeCard">
         <Row className="show-grid">
-          <Col className="resultFont" mdOffset={1} xsOffset={1} xs={5} md={5}>Galactic.com</Col>
+          <Col className="resultFont" mdOffset={1} xsOffset={1} xs={6} md={6}>Galactic.com</Col>
+          <Col className="resultInfo" xs={3} md={3}>
+            <div>Connections: 8</div>
+            <div>Connectors: 3</div>
+            <div><button>Add connection</button></div>
+          </Col>
         </Row>
       </Grid>
 
@@ -22,6 +27,11 @@ class Result extends React.Component {
           <Col className="connectionCard" mdOffset={1} xsOffset={1} xs={4} md={3}><span className="connectionCardText">DuckDuckGo</span></Col>
           <Col className="connectionCard" xs={4} md={3} mdOffset={0} xsOffset={1}><span className="connectionCardText">Google</span></Col>
           <Col className="connectionCard" xs={4} md={3} mdOffset={0} xsOffset={1}><span className="connectionCardText">Pinterest</span></Col>
+        </Row>
+        <Row className="show-grid">
+          <Col className="connectionCard" mdOffset={1} xsOffset={1} xs={4} md={3}><span className="connectionCardText">StumbleUpon</span></Col>
+          <Col className="connectionCard" xs={4} md={3} mdOffset={0} xsOffset={1}><span className="connectionCardText">Wikipedia</span></Col>
+          <Col className="connectionCard" xs={4} md={3} mdOffset={0} xsOffset={1}><span className="connectionCardText">SitesLike</span></Col>
         </Row>
       </Grid>
     </div>);
