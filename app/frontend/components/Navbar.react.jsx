255e6f6814e7cf97d0c5a0cd094870a53ed8a84b
Eoin McMillan <eoinmcmillan@gmail.com>
2016-05-20 21:32:06 -0700

    not sure what I'm committing here, just want to pull so your changes stop breaking my code

--- app/frontend/components/Navbar.react.jsx
+++ app/frontend/components/Navbar.react.jsx
@@ -4,6 +4,8 @@
 
 import React from "react";
 
+import LoginSignupModal from "./LoginSignupModal.react.jsx"
+
 export default class Navbar extends React.Component {
   render() {
     return (
@@ -14,7 +16,7 @@ export default class Navbar extends React.Component {
         <div className="navbar-header pull-right">
           <a href="connect"><button type="button" className="btn btn-default navbar-btn connect-icon-box"><img className="connect-icon" src="/img/galactic-icon-logo.png" /></button></a>
           <a href="user">
-            <button type="button" className="btn btn-default navbar-btn user-icon-box"><img className="user-icon" src="/img/most-beautiful-example-user.jpeg" /></button>
+            <button onClick={this.open} type="button" className="btn btn-default navbar-btn user-icon-box"><img className="user-icon" src="/img/most-beautiful-example-user.jpeg" /></button>
             </a>
         </div>
       </div>
