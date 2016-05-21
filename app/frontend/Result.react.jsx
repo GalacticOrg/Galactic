b62ec8f02c6a0b61bb0832ad44a587307aa4fadc
Eoin McMillan <eoinmcmillan@gmail.com>
2016-05-21 15:00:24 -0700

    safe commit on Result page before merging with jsx to js edit

--- app/frontend/Result.react.jsx
+++ app/frontend/Result.react.jsx
@@ -4,11 +4,26 @@
 import ReactDOM from "react-dom";
 import React from "react";
 import Navbar from "./components/Navbar.react.jsx"
+import { Grid, Row, Col, InputGroup, Glyphicon } from "react-bootstrap"
 
 class Result extends React.Component {
   render() {
     return (<div>
       <Navbar />
+
+      <Grid className="resultNodeCard">
+        <Row className="show-grid">
+          <Col className="resultFont" mdOffset={1} xsOffset={1} xs={5} md={5}>Galactic.com</Col>
+        </Row>
+      </Grid>
+
+      <Grid className="resultsSection">
+        <Row className="show-grid">
+          <Col className="connectionCard" mdOffset={1} xsOffset={1} xs={4} md={3}><span className="connectionCardText">DuckDuckGo</span></Col>
+          <Col className="connectionCard" xs={4} md={3} mdOffset={0} xsOffset={1}><span className="connectionCardText">Google</span></Col>
+          <Col className="connectionCard" xs={4} md={3} mdOffset={0} xsOffset={1}><span className="connectionCardText">Pinterest</span></Col>
+        </Row>
+      </Grid>
     </div>);
   }
 }
