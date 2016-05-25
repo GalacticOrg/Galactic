2f4f59343169d970b3b0284535c861112327553e
Jeff Jenkins <jenkinsjeffrey@gmail.com>
2016-05-25 11:43:37 -0700

    favicon and neo4j updates.

--- config/env/development.js
+++ config/env/development.js
@@ -5,7 +5,7 @@
 
 module.exports = {
   db: 'mongodb://localhost/galactic_development',
-  neo4jdb: 'http://neo4j:rambert@localhost:7474',
+  neo4jdb: 'http://neo4j:neo4j@localhost:7474',
   twitter: {
     clientID: process.env.TWITTER_CLIENT_KEY,
     clientSecret: process.env.TWITTER_CLIENT_SECRET,
