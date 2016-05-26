d684b597a8b4fef21f181b26f636b630d7b07bc4
Jeff Jenkins <jenkinsjeffrey@gmail.com>
2016-05-25 23:15:20 -0700

    Pushing production final.

--- config/env/production.js
+++ config/env/production.js
@@ -4,8 +4,8 @@
  */
 
 module.exports = {
-  db: process.env.MONGODB_URI || 'mongodb://localhost/galactic_prod',
-  neo4jdb: process.env.GRAPHENEDB_URL || 'http://neo4j:rambert@localhost:7474',
+  db: process.env.MONGODB_URI || 'mongodb://localhost/galactic_development',
+  neo4jdb: process.env.GRAPHENEDB_URL || 'http://neo4j:neo4j@localhost:7474',
   twitter: {
     clientID: process.env.TWITTER_CLIENT_KEY,
     clientSecret: process.env.TWITTER_CLIENT_SECRET,
