f2e0b4960f58f4b37d9c64f070940b9338bf0bd6
Jeff Jenkins <jenkinsjeffrey@gmail.com>
2016-06-03 12:11:45 -0700

    pushing code for browser sync.

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,27 @@
 const webpack = require('webpack');
 const env = process.env.NODE_ENV
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = {
     entry: {
       "home.js": env==='development'?[
-          'webpack-dev-server/client?http://0.0.0.0:8090', // WebpackDevServer host and port
-          'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+          'webpack-dev-server/client', // WebpackDevServer host and port
           './app/frontend/Home/' // Your appʼs entry point
       ]:'./app/frontend/Home/',
       "connect.js": env==='development'?[
-          'webpack-dev-server/client?http://0.0.0.0:8090', // WebpackDevServer host and port
-          'webpack/hot/only-dev-server',
+          'webpack-dev-server/client', // WebpackDevServer host and port
           './app/frontend/Connect/'
       ]:'./app/frontend/Connect/',
       "node.js": env==='development'?[
-          'webpack-dev-server/client?http://0.0.0.0:8090', // WebpackDevServer host and port
-          'webpack/hot/only-dev-server',
+          'webpack-dev-server/client', // WebpackDevServer host and port
           './app/frontend/Node/'
       ]:'./app/frontend/Node/',
       "user.js": env==='development'?[
-          'webpack-dev-server/client?http://0.0.0.0:8090', // WebpackDevServer host and port
-          'webpack/hot/only-dev-server',
+          'webpack-dev-server/client', // WebpackDevServer host and port
           './app/frontend/User/'
       ]:'./app/frontend/User/',
       "firehose.js": env==='development'?[
-          'webpack-dev-server/client?http://0.0.0.0:8090', // WebpackDevServer host and port
-          'webpack/hot/only-dev-server',
+          'webpack-dev-server/client', // WebpackDevServer host and port
           './app/frontend/Firehose/'
       ]:'./app/frontend/Firehose/',
     },
@@ -41,6 +37,23 @@ module.exports = {
     },
     devtool: env==='development'?'source-map':null,
     plugins: [
+      new BrowserSyncPlugin(
+        // BrowserSync options
+        {
+          // browse to http://localhost:3000/ during development
+          host: 'localhost',
+          port: 3000,
+          // proxy the node server
+          // through BrowserSync
+          proxy: 'http://localhost:3001/'
+        },
+        // plugin options
+        {
+          // prevent BrowserSync from reloading the page
+          // and let Webpack Dev Server take care of this
+          reload: true
+        }
+      ),
       new webpack.HotModuleReplacementPlugin(),
       new webpack.DefinePlugin({
         'process.env': {
