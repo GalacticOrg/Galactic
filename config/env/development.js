1e19b7de7117e89c74414de750d370a552028c6e
Jeff Jenkins <jenkinsjeffrey@gmail.com>
2017-01-03 20:32:49 -0800

    dev

---
+++ config/env/development.js
@@ -0,0 +1,24 @@
+
+/**
+ * Expose
+ */
+
+module.exports = {
+  db: 'mongodb://localhost/galactic_development',
+  neo4jdb: 'http://neo4j:neo4j@localhost:7474',
+  aws:{
+    key: process.env.AWS_KEY || '',
+    secret: process.env.AWS_SECRET || '',
+    imagebucket: process.env.IMAGE_BUCKET || ''
+  },
+  twitter: {
+    clientID: process.env.TWITTER_CLIENT_KEY,
+    clientSecret: process.env.TWITTER_CLIENT_SECRET,
+    callbackURL: ( process.env.ROOT_URL || 'http://localhost:3000' )+'/auth/twitter/callback',
+    scope: [
+      'email',
+      'user_about_me',
+      'user_friends'
+    ]
+  }
+};
