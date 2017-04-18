78b93c859c3ea54cad22d66daa58a15f2e1af68d
Jeff Jenkins <jenkinsjeffrey@gmail.com>
2017-04-18 15:15:52 -0700

    FA

--- app/controllers/appController.js
+++ app/controllers/appController.js
@@ -12,41 +12,41 @@ const home = function (req, res) {
   res.render('home');
 };
 
-module.exports.test = function (req, res) {
-  const url = 'https://newrepublic.com/article/142074/appalachia-needs-big-government';
-
-  parser(url, function (err, article){
-    if (err){
-      return res.send(err);
-    }
-    // res.json(article);
-    // return
-    // r
-    const page = Page.saveDiffBotResult(article.objects[0], req.user).then(function(result){
-      res.json(result);
-    });
-
-
-  });
-};
-
 // module.exports.test = function (req, res) {
-//   const pageId = 'CE212FC5-2CB9-4A19-BED2-9B7D19E1599E';
-//   // Connection.findAll({pageId:''})
-//   Page.findOne(
-//     { id:pageId, include:[{model: User}]}
-//   )
-//     .then(function(page){
-//       res.send(page)
-//     // page.getUser().getConnections().then(function(user){
-//     //   res.send({
-//     //     user,
-//     //     page
-//     //
-//     //   })
-//     // });
-//   })
+//   const url = 'https://newrepublic.com/article/142074/appalachia-needs-big-government';
+//
+//   parser(url, function (err, article){
+//     if (err){
+//       return res.send(err);
+//     }
+//     // res.json(article);
+//     // return
+//     // r
+//     const page = Page.saveDiffBotResult(article.objects[0], req.user).then(function(result){
+//       res.json(result);
+//     });
+//
+//
+//   });
 // };
+
+module.exports.test = function (req, res) {
+  const pageId = 'CE212FC5-2CB9-4A19-BED2-9B7D19E1599E';
+  // Connection.findAll({pageId:''})
+  // Page.findOne(
+  //   { id:pageId, include:[{model: User}]}
+  // )
+  //   .then(function(page){
+  //     res.send(page)
+  //   // page.getUser().getConnections().then(function(user){
+  //   //   res.send({
+  //   //     user,
+  //   //     page
+  //   //
+  //   //   })
+  //   // });
+  // });
+};
 
 module.exports.main = function (req, res) {
 
