7d6500511d7e94f1cd6907115cd8b3597f9c2a4b
Eoin McMillan <eoinmcmillan@gmail.com>
2017-04-20 12:49:13 -0700

    example connection and request tiles in feed

---
+++ static/js/main.js
@@ -0,0 +1,9 @@
+openRecommendationsModal = function() {
+	document.getElementById('requestModal').className += " displayRequestModal";
+	document.getElementById('modalMask').className += " displayRequestModal";
+}
+
+closeRecommendationsModal = function() {
+	document.getElementById('requestModal').classList.remove("displayRequestModal");
+	document.getElementById('modalMask').classList.remove("displayRequestModal");
+}
